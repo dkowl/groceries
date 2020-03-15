8ec00ed815de0583aca996f8a6aba647fa4e12e1
dkowl <arkenvoss.dk@gmail.com>
2020-03-15 16:07:05 +0100

    models updated

--- recipes/models.py
+++ recipes/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class FoodCategory(models.Model):
     category_name = models.CharField(max_length = 200)
     
+    def __str__(self):
+        return self.category_name
+    
 class Food(models.Model):
     food_name = models.CharField(max_length = 200)
     cost_per_kg = models.DecimalField(decimal_places = 2, max_digits = 9)
@@ -11,14 +14,23 @@ class Food(models.Model):
     carbs_per_100g = models.DecimalField(decimal_places = 1, max_digits = 4)
     protein_per_100g = models.DecimalField(decimal_places = 1, max_digits = 4)
     fat_per_100g = models.DecimalField(decimal_places = 1, max_digits = 4)
+    
+    def __str__(self):
+        return self.food_name
 
 class Recipe(models.Model):
     recipe_name = models.CharField(max_length = 200)
     description = models.TextField()
     creation_date = models.DateTimeField()
     
+    def __str__(self):
+        return self.recipe_name
+    
 class RecipeFood(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete = models.CASCADE)
     food = models.ForeignKey(Food, on_delete = models.PROTECT)
     quantity_grams = models.PositiveIntegerField()
+    
+    def __str__(self):
+        return self.recipe.recipe_name + ' - ' + self.food.food_name #Recipe.objects.get(id = self.recipe).recipe_name + ' - ' + Food.objects.get(id = self.food).food_name
     
