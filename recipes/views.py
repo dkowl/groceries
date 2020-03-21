0b1a431cc23efd3df15545724dba9e66ddecdddf
dkowl <arkenvoss.dk@gmail.com>
2020-03-21 19:41:57 +0100

    grocery lists!

--- recipes/views.py
+++ recipes/views.py
@@ -7,9 +7,53 @@ import traceback
 import datetime
 from decimal import Decimal, ROUND_HALF_UP
 from .forms import RecipeForm, FoodIdForm
-from .models import Food, Recipe, RecipeFood
+from .models import Food, Recipe, RecipeFood, FoodCategory
 
 logger = logging.getLogger(__name__)
+
+class FoodsView:
+    def __init__(self, recipeFoods=None):
+        kcal = Decimal(0)
+        carbs = Decimal(0)
+        protein = Decimal(0)
+        fat = Decimal(0)
+        price = Decimal(0)
+        pricePer1000Kcal = Decimal(0)
+        percentProtein = Decimal(0)
+
+        if recipeFoods:
+            for recipeFood in recipeFoods:
+                food = Food.objects.get(id=recipeFood.food.id)
+                multOf100g = Decimal(recipeFood.quantity_grams) / 100
+                kcal += food.kcal_per_100g * multOf100g
+                carbs += food.carbs_per_100g * multOf100g
+                protein += food.protein_per_100g * multOf100g
+                fat += food.fat_per_100g * multOf100g
+                price += food.cost_per_kg * multOf100g / 10
+
+        pricePer1000Kcal = (price / (kcal / Decimal(1000))) if kcal else Decimal(0)
+        percentProtein = (protein * 4 / (carbs * 4 + protein * 4 + fat * 9) * 100) if carbs or protein or fat else Decimal(0)
+
+        self.kcal = kcal.quantize(Decimal('1'), ROUND_HALF_UP)
+        self.carbs = carbs.quantize(Decimal('1'), ROUND_HALF_UP)
+        self.protein = protein.quantize(Decimal('1'), ROUND_HALF_UP)
+        self.fat = fat.quantize(Decimal('1'), ROUND_HALF_UP)
+        self.price = price.quantize(Decimal('0.01'), ROUND_HALF_UP)
+        self.pricePer1000Kcal = pricePer1000Kcal.quantize(Decimal('0.01'), ROUND_HALF_UP)
+        self.percentProtein = percentProtein.quantize(Decimal('1'), ROUND_HALF_UP)
+        
+        currency = " zł"
+
+        self.properties = []
+        self.properties.extend([
+            ('kcal', self.kcal),
+            ('carbs', str(self.carbs) + 'g'),
+            ('protein', str(self.protein) + 'g'),
+            ('fat', str(self.fat) + 'g'),
+            ('price', str(self.price) + currency),
+            ('price per 1000 kcal', str(self.pricePer1000Kcal) + currency),
+            ('% protein', str(self.percentProtein) + "%")
+        ])
 
 class RecipeView:         
         def __init__(self, recipe=None):
@@ -24,48 +68,51 @@ class RecipeView:
             self.name = recipe.recipe_name
             self.description = recipe.description
             self.foods = RecipeFood.objects.filter(recipe__pk=recipe.id)
-            kcal = Decimal(0)
-            carbs = Decimal(0)
-            protein = Decimal(0)
-            fat = Decimal(0)
-            price = Decimal(0)
-            pricePer1000Kcal = Decimal(0)
-            percentProtein = Decimal(0)
-            for recipeFood in self.foods:
-                food = Food.objects.get(id=recipeFood.food.id)
-                multOf100g = Decimal(recipeFood.quantity_grams) / 100
-                kcal += food.kcal_per_100g * multOf100g
-                carbs += food.carbs_per_100g * multOf100g
-                protein += food.protein_per_100g * multOf100g
-                fat += food.fat_per_100g * multOf100g
-                price += food.cost_per_kg * multOf100g / 10
-
-            pricePer1000Kcal = (price / (kcal / Decimal(1000))) if kcal else Decimal(0)
-            percentProtein = (protein * 4 / (carbs * 4 + protein * 4 + fat * 4) * 100) if carbs or protein or fat else Decimal(0)
-
-            kcal = kcal.quantize(Decimal('0.1'), ROUND_HALF_UP)
-            carbs = carbs.quantize(Decimal('0.1'), ROUND_HALF_UP)
-            protein = protein.quantize(Decimal('0.1'), ROUND_HALF_UP)
-            fat = fat.quantize(Decimal('0.1'), ROUND_HALF_UP)
-            price = price.quantize(Decimal('0.01'), ROUND_HALF_UP)
-            pricePer1000Kcal = pricePer1000Kcal.quantize(Decimal('0.01'), ROUND_HALF_UP)
-            percentProtein = percentProtein.quantize(Decimal('0.01'), ROUND_HALF_UP)
-
-            currency = " zł"
-
-            self.properties = []
-            self.properties.extend([
-                ('kcal', kcal),
-                ('carbs', str(carbs) + 'g'),
-                ('protein', str(protein) + 'g'),
-                ('fat', str(fat) + 'g'),
-                ('price', str(price) + currency),
-                ('price per 1000 kcal', str(pricePer1000Kcal) + currency),
-                ('% Protein', str(percentProtein) + "%")
-            ])
+            self.foodsView = FoodsView(self.foods)
 
         def __repr__(self):
             return self.name + str(self.properties)
+        
+class GroceryList:
+    def __init__(self, recipeFoods):
+        #consolidate by category->(food.id->weight)
+        categories = {}
+        for recipeFood in recipeFoods:
+            food = Food.objects.get(id=recipeFood.food_id)
+            categoryId = food.food_category.id
+            if not categoryId in categories:
+                categories[categoryId] = {}
+
+            if food.id in categories[categoryId]:
+                categories[categoryId][food.id] += recipeFood.quantity_grams
+            else:
+                categories[categoryId][food.id] = recipeFood.quantity_grams
+        
+        self.categories = []
+        for categoryId in categories.keys():
+            category = (FoodCategory.objects.get(id=categoryId).category_name, [])
+            for foodId, foodWeight in categories[categoryId].items():
+                foodName = Food.objects.get(id=foodId).food_name
+                category[1].append((foodName, foodWeight))
+            self.categories.append(category)
+
+        logger.debug(self.categories)
+
+class Basket:
+    def __init__(self, recipeIds):
+        self.recipeIds = recipeIds
+        self.recipeNames = []
+        for recipeId in recipeIds:
+            self.recipeNames.append(Recipe.objects.filter(id=recipeId).values('recipe_name')[0]['recipe_name'])
+
+        recipeFoods = []
+        for recipeId in recipeIds:
+            recipeFoods.extend(RecipeFood.objects.filter(recipe__pk=recipeId))
+        self.foodsView = FoodsView(recipeFoods)
+
+        self.groceryList = GroceryList(recipeFoods)
+
+
 
 # Create your views here.
 def index(request):
@@ -78,27 +125,13 @@ def index(request):
         except:
             logger.error(traceback.format_exc())
 
-    basket_props = {
-        "name": "Basket",
-        "kcal": 0,
-        "carbs": 0,
-        "protein": 0,
-        "fat": 0,
-        "price": 0,
-        "price per 1000 kcal": 0,
-        "% protein": 0
-    }
-    
-    property_order = [
-        "kcal", "carbs", "protein", "fat", "price", "price per 1000 kcal", "% protein"
-    ]
-    
-    
-        
-    basket = RecipeView()
-    basket.name = "Basket"
-    for property in property_order:
-        basket.properties.append((property, basket_props[property]))
+    recipeIds = request.GET.getlist('r')
+    if request.GET:
+        try:
+            recipeIds = request.GET.getlist('r')
+        except:
+            logger.error(traceback.format_exc())
+    basket = Basket(recipeIds)
        
     recipes = Recipe.objects.all()
     recipeViews = []
